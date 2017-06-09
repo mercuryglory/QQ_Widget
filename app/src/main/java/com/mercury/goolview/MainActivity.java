1d86f969fd61641ab0fe9119a4a17ac9f34fe6a5
mercuryglory <wzhseu@163.com>
2017-06-09 23:00:42 +0800

    分离demo和library

--- app/src/main/java/com/mercury/goolview/MainActivity.java
+++ app/src/main/java/com/mercury/goolview/MainActivity.java
@@ -1,7 +1,7 @@
 package com.mercury.goolview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.mercury.goolview.ui.GooView;
 
