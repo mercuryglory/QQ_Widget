f6b5a74115be874d679c9ef1b6c8463338501c67
mercuryglory <wzhseu@163.com>
2017-06-20 16:23:04 +0800

    fix:事件分发，子view在滑动的过程中Listview还可以上下滑动

--- swipelayout/src/main/java/com/mercury/swipelayout/ui/SwipeLayout.java
+++ swipelayout/src/main/java/com/mercury/swipelayout/ui/SwipeLayout.java
@@ -6,11 +6,12 @@ import android.support.v4.view.MotionEventCompat;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+
+import com.mercury.swipelayout.LogUtil;
 
 /**
  * Created by Mercury on 2016/8/12.
@@ -25,7 +26,7 @@ public class SwipeLayout extends FrameLayout {
     private int            mHeight;     //该控件显示在屏幕内的高度
     private int            mRange;      //拖拽范围,也就是后布局的宽度
 
-    boolean isOpen = false;   //控件默认的状态(后布局是否显示),关闭
+    boolean isOpen = false;             //控件默认的状态(后布局是否显示),关闭
     private Status status = Status.Close;
 
     public enum Status {
@@ -74,8 +75,11 @@ public class SwipeLayout extends FrameLayout {
             return mRange;
         }
 
+        //限定子view滑动到的边界（左端的x坐标）
         @Override
         public int clampViewPositionHorizontal(View child, int left, int dx) {
+            getParent().requestDisallowInterceptTouchEvent(true);
+
             //第二个参数left是当前滑动的view的左端滑动到的位置的x坐标
             if (child == mFrontLayout) {
                 //当前滑动发生在前布局，控制其拖拽范围
@@ -98,9 +102,8 @@ public class SwipeLayout extends FrameLayout {
         @Override
         public void onViewPositionChanged(View changedView, int left, int top, int dx, int dy) {
             if (changedView == mFrontLayout) {
-                //如果前布局位置发生变化,传递给后布局
-//                int i = mBackLayout.getLeft();
-                ViewCompat.offsetLeftAndRight(mBackLayout,dx);
+                //如果前布局位置发生变化,应该让后布局的位置也发生相应的变化，反之亦然
+                ViewCompat.offsetLeftAndRight(mBackLayout, dx);
             } else if (changedView == mBackLayout) {
                 ViewCompat.offsetLeftAndRight(mFrontLayout, dx);
             }
@@ -109,8 +112,10 @@ public class SwipeLayout extends FrameLayout {
             dispatchChangeEvent();
         }
 
+        //子view松手时回调的方法，参数提供了触摸点离开屏幕时各方向每秒运动的速率
         @Override
         public void onViewReleased(View releasedChild, float xvel, float yvel) {
+            LogUtil.logE("release", xvel + "-----" + yvel);
             if (xvel == 0 && mFrontLayout.getLeft() < -mRange / 2.0) {
                 open();
             } else if (xvel < 0) {
@@ -118,6 +123,7 @@ public class SwipeLayout extends FrameLayout {
             } else {
                 close();
             }
+            getParent().requestDisallowInterceptTouchEvent(false);
         }
     };
 
@@ -143,7 +149,7 @@ public class SwipeLayout extends FrameLayout {
 
     private Status updateStatus() {
         int left = mFrontLayout.getLeft();
-        Log.e("status", left + "");
+        LogUtil.logE("status", left + "");
         if (left == -mRange) {
             return Status.Open;
         } else if (left == 0) {
@@ -160,12 +166,16 @@ public class SwipeLayout extends FrameLayout {
 
     public void open(boolean isSmooth) {
         if (isSmooth) {
-            //触发平滑动画
+            LogUtil.logE("smooth", "true");
+            //用来完成手势结束后的惯性操作
             if (mDragHelper.smoothSlideViewTo(mFrontLayout, -mRange, 0)) {
+                //如果手势的速度不是很快，即时未滑动到坐标，子view也会自动滑动到位置
                 //drawChild->child.draw->computeScroll
                 ViewCompat.postInvalidateOnAnimation(this);
+                LogUtil.logE("smooth", "slide");
             }
         } else {
+            //以布局的方式直接更新view的位置
             isOpen = true;
             layoutContent(isOpen);
         }
@@ -183,7 +193,6 @@ public class SwipeLayout extends FrameLayout {
                 ViewCompat.postInvalidateOnAnimation(this);
             }
         } else {
-
             isOpen = false;
             layoutContent(isOpen);
         }
@@ -191,7 +200,9 @@ public class SwipeLayout extends FrameLayout {
 
     @Override
     public void computeScroll() {
+        //dragHelper内部使用的是 mScroller.startScroll,需要重写该方法
         super.computeScroll();
+        //如果返回true,就说明下一帧仍然要调用，就是惯性动画还在继续，要更新视图
         if (mDragHelper.continueSettling(true)) {
             ViewCompat.postInvalidateOnAnimation(this);
         }
@@ -209,6 +220,7 @@ public class SwipeLayout extends FrameLayout {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        //ViewDragHelper会监听ViewGroup的onTouchEvent,所以必须注册才能起到效果
         try {
             mDragHelper.processTouchEvent(event);
         } catch (Exception e) {
@@ -228,14 +240,14 @@ public class SwipeLayout extends FrameLayout {
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
-        //指定子控件的位置
 
+        //指定控件中前布局和后布局的位置
         layoutContent(isOpen);
     }
 
 
     /**
-     * 根据控件的开关状态布局内容
+     * 根据控件的开关状态布局内容,内容区域是子view相对于parent view，也就是本控件内的位置
      * @param isOpen
      */
     private void layoutContent(boolean isOpen) {
@@ -254,13 +266,14 @@ public class SwipeLayout extends FrameLayout {
     //计算前布局矩形区域
     private Rect computeFrontRect(boolean isOpen) {
         int left = 0;
+        //如果后布局完全侧滑出来
         if (isOpen) {
             left = -mRange;
         }
         return new Rect(left, 0, left + mWidth, 0 + mHeight);
     }
 
-    //计算后布局矩形区域
+    //根据前布局的参数计算后布局矩形区域
     private Rect computeBackRect(Rect frontRect) {
         int left = frontRect.right;
         return new Rect(left, 0, left + mRange, 0 + mHeight);
