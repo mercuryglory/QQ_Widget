7bd6d0dbeb4485f5d97a2328749070560d478d30
mercuryglory <wzhseu@163.com>
2017-07-25 18:44:08 +0800

    upgrade: triangle view

---
+++ app/src/main/java/com/demo/widget/TriangleView.java
@@ -0,0 +1,100 @@
+package com.demo.widget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by wang.zhonghao on 2017/7/25.
+ */
+
+public class TriangleView extends View {
+
+    private Paint mPaint;
+
+    public TriangleView(Context context) {
+        this(context, null);
+    }
+
+    public TriangleView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public TriangleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        mPaint = new Paint();
+        mPaint.setColor(Color.RED);
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mPaint.setStyle(Paint.Style.FILL);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int status = getStatusBarHeight(getContext());
+        canvas.translate(0, -status);
+        int[] points = new int[2];
+        getLocationInWindow(points);
+
+        int left = points[0];
+        int top = points[1];
+
+        int width = this.getWidth();
+        int height = this.getHeight();
+
+        int left1 = this.getLeft();
+        int right1 = this.getRight();
+
+
+        Path path = new Path();
+        path.moveTo(0, 0);
+        path.lineTo(width, 0);
+        path.lineTo(width, height);
+//        path.moveTo(800, 400);// 此点为多边形的起点
+//        path.lineTo(890, 400);
+//        path.lineTo(890, 490);
+        path.close();
+        canvas.drawPath(path, mPaint);
+    }
+
+    public int getStatusBarHeight(Context context) {
+        int statusBarHeight = -1;
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen",
+                "android");
+        if (resourceId > 0) {
+            statusBarHeight = context.getResources().getDimensionPixelSize(resourceId);
+        }
+
+        return statusBarHeight;
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width = 0;
+        int height = 0;
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        if (widthMode == MeasureSpec.EXACTLY) {
+            width = widthSize;
+        } else if (widthMode == MeasureSpec.AT_MOST) {
+            width = 90;
+        }
+        if (heightMode == MeasureSpec.EXACTLY) {
+            height = heightSize;
+        } else if (heightMode == MeasureSpec.AT_MOST) {
+            height = 90;
+        }
+        setMeasuredDimension(width, height);
+    }
+}
